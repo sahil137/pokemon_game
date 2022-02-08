94134e09233bf0a28d9aeb4ca5d8bc4b6e76a67d
Sahil Saini <sahilsaini137@gmail.com>
2022-02-08 23:55:38 +0530

    simple pokemon game made using react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Pokegame from './Pokegame';
+import './App.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Pokegame />
+      </div>
+    );
+  }
+}
+
+export default App;
